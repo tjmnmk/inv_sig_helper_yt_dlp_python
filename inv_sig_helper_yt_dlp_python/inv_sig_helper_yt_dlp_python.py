5dc7c0caa8456435c6064bbd910a77a2cba95553
Adam Bambuch <bambuch@vshosting.cz>
2025-04-07 15:16:52 +0200

    set tcp_nodelay and reuseaddr

--- inv_sig_helper_yt_dlp_python/inv_sig_helper_yt_dlp_python.py
+++ inv_sig_helper_yt_dlp_python/inv_sig_helper_yt_dlp_python.py
@@ -17,6 +17,10 @@ def handle_client(conn, addr, player):
     
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # set nodelay
+    server.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+    # allow reuse of the socket
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind((Config().get_host(), Config().get_port()))
     server.listen()
     logger.info(f"[+] Server listening on {Config().get_host()}:{Config().get_port()}")
